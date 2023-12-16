e4ad4f44377f02ef3af889a315d5fb827e9eb64e
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-16 13:57:08 +0900

    rename: 大文字と小文字の変換はGitのdiffに出んかったらしい

--- app/src/main/java/com/presio/memopad/controller/UserController.java
+++ app/src/main/java/com/presio/memopad/controller/UserController.java
@@ -3,11 +3,14 @@ package com.presio.memopad.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.presio.memopad.error.UserPasswordIsIncorrectException;
+import com.presio.memopad.model.AuthSession;
 import com.presio.memopad.model.User;
 import com.presio.memopad.request.SignUpRequest;
 import com.presio.memopad.response.UserResponse;
@@ -22,6 +25,11 @@ public class UserController {
 
   @Autowired
   private UserService userService;
+
+  @Autowired
+  private StringRedisTemplate stringRedisTemplate;
+
+  ObjectMapper mapper = new ObjectMapper();
 
   @GetMapping("/user")
   public List<UserResponse> getUsers() {
@@ -39,6 +47,14 @@ public class UserController {
     user.setEmail(input.getEmail());
     user.setPassword(input.getPassword());
     UserResponse res = new UserResponse(userService.createUser(user));
+
+    AuthSession session = new AuthSession(user.getId());
+    try {
+      stringRedisTemplate.opsForValue().set(session.getSessionId(), mapper.writeValueAsString(session));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
     return ResponseEntity.ok(res);
     // User res = userService.createUser(user);
   }
@@ -50,6 +66,14 @@ public class UserController {
       throw new UserPasswordIsIncorrectException(input.getPassword());
     }
     UserResponse res = new UserResponse(user);
+
+    AuthSession session = new AuthSession(user.getId());
+    try {
+      stringRedisTemplate.opsForValue().set(session.getSessionId(), mapper.writeValueAsString(session));
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
     return ResponseEntity.ok(res);
   }
 
